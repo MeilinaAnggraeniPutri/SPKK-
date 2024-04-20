3cf0f3d8c00867de2f8595b1eb13e67fb97df5d4
MeilinaAnggraeniPutri <meilinaanggraeniputri@gmail.com>
2024-04-21 02:17:51 +0700

    Menambah halaman kriteria

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,19 +6,11 @@ const Category = require('../models/category');
 class categoryController{
 
     static async index(req, res, next){
-        // let page = parseInt(req.query.page) || 1;
-        // if(page < 1) page = 1;
-        // const limit = 5;
-        // const skip = (page - 1) * limit;
-        
-        // const totalCategory = await Category.countDocuments();
-        // const totalPages = Math.ceil(totalCategory / limit);
-        
-        // const category = await Category.find().skip(skip).limit(limit);
         const category = await Category.find();
         
-        res.render('penilaian/addCategory', { category });
+        res.render('category/index', { category });
     }
+
     static async createCategory(req, res, next){
         const {name} = req.body;
         const category = new Category({name});
