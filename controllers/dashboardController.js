d77996bbaa654bd873b4f68fdbf87bee44e73979
MeilinaAnggraeniPutri <meilinaanggraeniputri@gmail.com>
2024-04-26 01:04:04 +0700

    Memperbarui Dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,17 +1,30 @@
 const Pegawai = require('../models/pegawai');
 const Kategori = require('../models/category');
+const Penilaian = require('../models/penilaian');
+const Jabatan = require('../models/jabatan');
 
 class dashboardController {
     static async index(req, res, next) {
-        var username = 'Guest';
-        var userId = '010';
-        if (req.session.isLogin) {
-            username = req.session.username;
-            userId = req.session.userId;
+        try {
+            var username = 'Guest';
+            var userId = '010';
 
-            res.render('dashboard', {username, userId});
-        } else {
-            res.redirect('/login');
+            
+            if (req.session.isLogin) {
+                username = req.session.username;
+                userId = req.session.userId;
+
+                const totalPegawai = await Pegawai.countDocuments();
+                const totalKategori = await Kategori.countDocuments();
+                const totalPenilaian = await Penilaian.countDocuments();
+                const totalJabatan = await Jabatan.countDocuments()
+
+                res.render('dashboard', {username, userId, totalPegawai, totalKategori, totalPenilaian, totalJabatan});
+            } else {
+                res.redirect('/login');
+            }
+        } catch (e) {
+            next(e);
         }
     } 
 }
