966d7d40667d1fa11dbfbfed2671c86b50215f90
MeilinaAnggraeniPutri <meilinaanggraeniputri@gmail.com>
2024-04-27 23:26:20 +0700

    Menambahkan fitur update category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -63,6 +63,52 @@ class categoryController{
         }
     }
 
+    static async editCategory(req, res, next) {
+        try {
+          if (req.session.isLogin) {
+            const categoryId = req.params.id;
+            const category = await Category.findById(categoryId);
+            res.render('category/editCategory', { category });
+         } else {
+            res.redirect('/login');
+          }
+        } catch (e) {
+          next(e);
+        }
+    }
+
+    static async updateCategory(req, res, next){
+        try {
+            const {idCategory, name, categoryType, weight} = req.body;
+            var subCriterias = [];
+            var maxValues = [];
+            var documents = [];
+    
+            const banyakSubCriteria = req.body['banyakSubCriteria'];
+            const banyakDokumen = req.body['banyakDokumen'];
+    
+            for (var i = 0; i < banyakSubCriteria; i++) {
+                var subCriteria = req.body[`subCriterias-${i}`];
+                var maxValue = req.body[`maxValues-${i}`];
+    
+                subCriterias.push(subCriteria);
+                maxValues.push(maxValue);
+            }
+    
+            for (var i = 0; i < banyakDokumen; i++) {
+                var document = req.body[`document-${i}`];
+    
+                documents.push(document);
+            }
+    
+            const category = await Category.findByIdAndUpdate(idCategory, {name, subCriterias, maxValues, documents, categoryType, weight});
+
+            res.redirect(`/category`);
+        } catch (e) {
+            next(e);
+        }
+    }
+
     static async deleteCategory(req, res, next){
         const { id } = req.params;
         await Category.findByIdAndDelete(id);
